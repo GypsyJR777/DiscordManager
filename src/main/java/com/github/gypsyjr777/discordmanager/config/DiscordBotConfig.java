e47368c50aabd2caa7c0420c46551a5b3d4dbb77
GypsyJR777 <ivan.demidov.01@list.ru>
2024-03-05 23:04:48 +0300

    return spring-boot; change creating beans

--- src/main/java/com/github/gypsyjr777/discordmanager/config/DiscordBotConfig.java
+++ src/main/java/com/github/gypsyjr777/discordmanager/config/DiscordBotConfig.java
@@ -14,32 +14,36 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
-@ComponentScan("com.github.gypsyjr777.discordmanager")
-@AutoConfiguration
+@EnableJpaRepositories(basePackages = "com.github.gypsyjr777.discordmanager.repository")
 public class DiscordBotConfig {
-    @Autowired
-    private ApplicationContext context;
-
     @Value("${discordToken}")
     private String token;
 
     @Bean
-    public JDA jda() throws InterruptedException {
+    public JDA jda(
+            ReadyEventListener readyEventListener, GuildVoiceEvent guildVoiceEvent,
+            GuildMembersEvent guildMembersEvent,
+            SlashCommandInteraction slashCommandInteraction, GuildMessageReactionEvent guildMessageReactionEvent,
+            GuildMessageEvent guildMessageEvent,
+            NewGuildEvent newGuildEvent,
+            GuildNewsEvent guildNewsEvent
+            ) throws InterruptedException {
 
         return JDABuilder.createDefault(token)
                 .enableIntents(GatewayIntent.MESSAGE_CONTENT) // enables explicit access to message.getContentDisplay()
                 .setActivity(Activity.playing("Bot`s simple life"))
                 .setEventManager(new AnnotatedEventManager())
-                .addEventListeners(new ReadyEventListener(context))
-                .addEventListeners(new GuildVoiceEvent(context))
-                .addEventListeners(new GuildMembersEvent(context))
-                .addEventListeners(new SlashCommandInteraction(context))
-                .addEventListeners(new GuildMessageReactionEvent(context))
-                .addEventListeners(new GuildMessageEvent(context))
-                .addEventListeners(new NewGuildEvent(context))
-                .addEventListeners(new GuildNewsEvent(context))
+                .addEventListeners(readyEventListener)
+                .addEventListeners(guildVoiceEvent)
+                .addEventListeners(guildMembersEvent)
+                .addEventListeners(slashCommandInteraction)
+                .addEventListeners(guildMessageReactionEvent)
+                .addEventListeners(guildMessageEvent)
+                .addEventListeners(newGuildEvent)
+                .addEventListeners(guildNewsEvent)
                 .enableIntents(GatewayIntent.GUILD_MEMBERS)
                 .enableIntents(GatewayIntent.GUILD_PRESENCES)
                 .enableIntents(GatewayIntent.GUILD_MESSAGES)
--- src/main/java/com/github/gypsyjr777/discordmanager/event/GuildMessageReactionEvent.java
+++ src/main/java/com/github/gypsyjr777/discordmanager/event/GuildMessageReactionEvent.java
@@ -5,25 +5,24 @@ import com.github.gypsyjr777.discordmanager.entity.DiscordRole;
 import com.github.gypsyjr777.discordmanager.exception.NullRoleException;
 import com.github.gypsyjr777.discordmanager.service.GuildService;
 import com.github.gypsyjr777.discordmanager.service.RoleService;
-import com.github.gypsyjr777.discordmanager.utils.BasicUtils;
-import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
-import net.dv8tion.jda.api.entities.emoji.Emoji;
+import com.github.gypsyjr777.discordmanager.service.BasicUtilsService;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.hooks.SubscribeEvent;
-import org.springframework.context.ApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GuildMessageReactionEvent extends ListenerAdapter {
     private final GuildService guildService;
     private final RoleService roleService;
-    private final BasicUtils utils;
+    private final BasicUtilsService utils;
 
-    public GuildMessageReactionEvent(ApplicationContext context) {
-        this.guildService = context.getBean(GuildService.class);
-        this.roleService = context.getBean(RoleService.class);
-        this.utils = context.getBean(BasicUtils.class);
+    @Autowired
+    public GuildMessageReactionEvent(GuildService guildService, RoleService roleService, BasicUtilsService utils) {
+        this.guildService = guildService;
+        this.roleService = roleService;
+        this.utils = utils;
     }
 
     @Override
